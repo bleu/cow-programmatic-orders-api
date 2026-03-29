c471251e51a08ef025df9637e64cd1eb65adfbdb
Jefferson Bastos <jeff.junior98@gmail.com>
2026-03-29 20:15:44 -0300

    feat: add Gnosis order type handler addresses

--- src/utils/order-types.ts
+++ src/utils/order-types.ts
@@ -17,8 +17,14 @@ const HANDLER_MAP: Record<number, Record<string, OrderType>> = {
     "0x519ba24e959e33b3b6220ca98bd353d8c2d89920": "PerpetualSwap",
     "0xdaf33924925e03c9cc3a10d434016d6cfad0add5": "GoodAfterTime",
     "0x812308712a6d1367f437e1c1e4af85c854e1e9f6": "TradeAboveThreshold",
-  },
-  100: {}, // Gnosis Chain
+  }, // Mainnet
+  100: {
+    "0x6cf1e9ca41f7611def408122793c358a3d11e5a5": "TWAP",
+    "0x412c36e5011cd2517016d243a2dfb37f73a242e7": "StopLoss",
+    "0x519ba24e959e33b3b6220ca98bd353d8c2d89920": "PerpetualSwap",
+    "0xdaf33924925e03c9cc3a10d434016d6cfad0add5": "GoodAfterTime",
+    "0x812308712a6d1367f437e1c1e4af85c854e1e9f6": "TradeAboveThreshold",
+  }, // Gnosis Chain
   42161: {}, // Arbitrum One
 };
 
